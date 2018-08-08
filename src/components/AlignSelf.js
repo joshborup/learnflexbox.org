67c388c7112f87c8cc6913b6356b403f88367f28
joshborup <joshborup@gmail.com>
2018-08-08 11:29:58 -0700

    fixed radio buttons

--- src/components/AlignSelf.js
+++ src/components/AlignSelf.js
@@ -3,26 +3,26 @@ import React from 'react';
 const AlignSelf = (props) => {
     console.log(props)
     return (
-        <div className='flex-direction' onChange={(e)=>props.changeHandler(props.propName, e.target.value)} value={props.alignSelf}>
+        <div className='flex-direction' onChange={(e)=>props.changeHandler(e.target.name, e.target.value)} value={props.alignSelf}>
             <h3>Align Self</h3>
                 <div>
                 <label>
-                    <input type='radio' name='alignSelf' defaultChecked={true} value='stretch' />
+                    <input type='radio' name={props.propName} checked={props.alignSelf === 'stretch' ? true : false} value='stretch' />
                     Stretch (Default)</label>
                 </div>
                 <div>
                 <label>
-                    <input type='radio' name='alignSelf' value='center' />
+                    <input type='radio' name={props.propName} checked={props.alignSelf === 'center' ? true : false}  value='center' />
                     Center</label>
                 </div>
                 <div>
                 <label>
-                    <input type='radio' name='alignSelf' value='flex-start' />
+                    <input type='radio' name={props.propName} checked={props.alignSelf === 'flex-start' ? true : false}  value='flex-start' />
                     Flex-start</label>
                 </div>
                 <div>
                 <label>
-                    <input type='radio' name='alignSelf' value='flex-end' />
+                    <input type='radio' name={props.propName} checked={props.alignSelf === 'flex-end' ? true : false}  value='flex-end' />
                     Flex-end</label>
                 </div>
         </div>
